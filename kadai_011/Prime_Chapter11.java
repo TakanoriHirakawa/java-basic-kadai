aba6c4893f4196bdad162cd7f8b6d8fa716292a8
TakanoriHirakawa <136884342+TakanoriHirakawa@users.noreply.github.com>
2023-06-19 20:26:43 +0900

    update Prrime_Cahpter11.java_rev3

--- kadai_011/Prime_Chapter11.java
+++ kadai_011/Prime_Chapter11.java
@@ -3,16 +3,20 @@ package kadai_011;
 public class Prime_Chapter11 {
 
 	public static void main(String[] args) {
-		boolean[] prime=new boolean[101];
+		//2～100まで、素数であるかを判定する配列（便宜上101）
+		boolean[] primeFlg=new boolean[101];
+		
 		
 		for(int i=2;i<=100;i++) {
-			prime[i]=true;
+			//2～100まで全てを素数であると仮定
+			primeFlg[i]=true;
 			for (int j=2;j<i;j++) {
+				//2～100までの数についてi/ （2～i）までの数の剰余が0となった場合は、falseにする。
 				if (i%j==0 ) {
-					prime[i]=false;
+					primeFlg[i]=false;
 				} 	
 			}
-			if (prime[i]==true) {
+			if (primeFlg[i]==true) {
 				System.out.println(i);
 			}
 		}
